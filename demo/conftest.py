3958bdb6604fabda52f2827e3cec2881ea2024e6
tanjf1 <1456181121@qq.com>
2019-11-13 17:30:39 +0800

    第liu次修改

--- demo/conftest.py
+++ demo/conftest.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def driver():
-    driver=webdriver.Chrome('../chromedirve78/chromedriver.exe')
+    driver=webdriver.Chrome('../chromedirve78/chromedriver.exe')        #打开谷歌浏览器
     #调整浏览器大小最大化
     #driver.maximize_window()
-    yield driver
-    driver.close()
+    yield driver        #之后操作一下命令
+    driver.close()      #关闭浏览器
--- demo/test_element.py
+++ demo/test_element.py
@@ -7,17 +7,17 @@ from selenium.webdriver.common.keys import Keys
 
 
 def test_input(driver):
-    driver.get('http://ui.yansl.com/#/input')
-    sleep(2)
+    driver.get('http://ui.yansl.com/#/input')    #打开url
+    sleep(2)       #停顿2秒
 
-    input = driver.find_element_by_xpath("//input[@name='t1']")
+    input = driver.find_element_by_xpath("//input[@name='t1']")     #找到需要操作的位置，放到input里
 
     # 清空
-    input.clear()
+    input.clear()   #清空操作位置的内容
 
     # 输入
-    input.send_keys("我是谁?我在哪?我该干什么?")
-    sleep(2)
+    input.send_keys("我是谁?我在哪?我该干什么?")   #输入内容"我是谁?我在哪?我该干什么?"
+    sleep(2)    #停顿2秒
 
 
 # 点击
@@ -31,7 +31,7 @@ def test_radio(driver):
     # radio.clear()
 
     # 点击
-    radio.click()
+    radio.click()   #点击对应的位子
     sleep(2)
 
 
@@ -42,9 +42,9 @@ def test_checkbox(driver):
 
     checkbox = driver.find_element_by_xpath("(//span[@class='el-checkbox__inner'])[1]")
 
-    attributtr = checkbox.get_attribute('class')
-    if not attributtr == 'el-checkbox__input is-checked':
-        checkbox.click()
+    attributtr = checkbox.get_attribute('class')    #判断属性值
+    if not attributtr == 'el-checkbox__input is-checked':       #如果不满足条件
+        checkbox.click()        #点击
         sleep(5)
 
     heckbox = driver.find_element_by_xpath("(//span[@class='el-checkbox__inner'])[1]")
@@ -67,8 +67,8 @@ def test_select(driver):
     sleep(2)
 
     option = driver.find_element_by_xpath('(//span[text()="龙须面"])[last()]')
-    actions = ActionChains(driver)
-    actions.move_to_element(option).perform()
+    actions = ActionChains(driver)          #导入，鼠标键盘操作都有，键盘操作26个字母
+    actions.move_to_element(option).perform()       #鼠标移动，命令组合，perform运行命令
     sleep(2)
     option.click()
     sleep(2)
@@ -85,7 +85,7 @@ def test_time(driver):
     t1.clear()
 
     # 输入
-    t1.send_keys("14:17:17")
+    t1.send_keys("14:17:17")        #输入内容（"14:17:17"）
     sleep(2)
 
 
@@ -101,7 +101,7 @@ def test_upload(driver):
     upload.clear()
 
     # 输入
-    upload.send_keys("C:\\Users\\guoya\\Desktop\\微信图片_20191028085516.jpg")
+    upload.send_keys("C:\\Users\\guoya\\Desktop\\微信图片_20191028085516.jpg")      #输入图片位置，路径单\改成\\（双\）
     sleep(2)
 
 
@@ -115,7 +115,7 @@ def test_upload1(driver):
     # 清空
     upload1.click()
     sleep(2)
-
+#1
     autoit.win_wait("打开", 10)
     sleep(1)
     # autoit.control_send("打开", "Edit1", os.path.abspath(file_path))
@@ -123,6 +123,7 @@ def test_upload1(driver):
     sleep(2)
     autoit.control_click("打开", "Button1")
     sleep(5)
+#2      #1-#2 ，点击后通过windows路径 上传图片，内容仅需改变文件路径，其他固定格式，无需改动
 
 def test_alert(driver):
     driver.get("http://192.168.1.128:8082/xuepl/demo.html")
@@ -162,3 +163,4 @@ def test_windows(driver):
         sleep(2)
         if driver.title.__contains__("京东"):
             break
+
